b9b0fc02b521cc37e7220f7a0ecb874512cb41e0
Daniel Raoli <danielraoli074@gmail.com>
2023-10-23 14:36:04 -0300

    Adicionar arquivos de projeto.

---
+++ LeituraDeInteiros/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LeituraDeInteiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\dnlus\OneDrive\Documentos\Numeros.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    int contador = 0;
+                    int sum = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        var num = int.Parse(sr.ReadLine());
+                        sum += num;
+                        contador++;
+
+                    }
+
+                    var average = sum / contador;
+                    Console.WriteLine("A média é: " + average);
+
+                }
+            }catch (IOException ex) 
+            {
+                Console.WriteLine("Erro ao tentar abrir o arquivo");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
